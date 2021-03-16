f47c0e4584abaa430a4d3e61295a0d2b0de2a90b
rachel <baruh.rahel@gmail.com>
2021-03-16 23:58:01 +0200

    3-1

--- react-exams/src/App.js
+++ react-exams/src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
+import Boxes from "./Components/Boxes3-1/Boxes"
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        CLICK!
-        <
-        /a> <
-        /header> <
-        /div>
+    return (
+        <Boxes/>
     );
 }
 
---
+++ react-exams/src/components/Boxes3-1/Box3.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Box4 from "./Box4"
+
+const Box3 = () => {
+    return ( <div className = "box3" ><Box4/> <Box4/> </div>
+    )
+}
+export default Box3;
