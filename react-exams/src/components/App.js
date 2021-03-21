130392d592504d783510f7f327f85d826c9489d4
rachel <baruh.rahel@gmail.com>
2021-03-22 00:32:02 +0200

    11-1

---
+++ react-exams/src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CustomButton from "./CustomButton/CustomButton";
+
+class App extends React.Component {
+  state = {color: ''};
+
+  onClickButton = (color) => {
+    this.setState({color: color});
+  }
+
+  renderButtons = () => {
+    const colors = ["blue", "red", "yellow"];
+    return colors.map(color => {
+      return <CustomButton onClick={this.onClickButton} color={color} />;
+    })
+  }
+
+
+
+  /* shows colors as a text */
+  render() {
+    return (
+      <div>
+        {this.renderButtons()}
+        <h1>The color selected is : {this.state.color}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
