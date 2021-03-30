201db68645d5f8227152876ae1cbb5808880dc73
rachel <baruh.rahel@gmail.com>
2021-03-30 16:56:20 +0300

    19-1

---
+++ react-exams/src/components/Hooks/19-1/App.js
@@ -0,0 +1,11 @@
+// import Todo from "./components/Hooks/18-2/Todo.component";
+import StarApi from "./components/Hooks/19-1/StarApi";
+function App() {
+  return (
+    <div className="App">
+      <StarApi />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-exams/src/components/Hooks/19-1/backaup-App.js
@@ -0,0 +1,11 @@
+// import Todo from "./components/Hooks/18-2/Todo.component";
+import StarwarApi from "./components/Hooks/19-1/StarwarApi";
+function App() {
+  return (
+    <div className="App">
+      <StarwarApi />
+    </div>
+  );
+}
+
+export default App;
